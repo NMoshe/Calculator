dfdf86ab814cfb349b874115e4f43436b78a083c
Moshe <nicholas.namrot@gmail.com>
2020-01-05 16:42:07 -0500

    Basic structure applied

---
+++ Calculator.js
@@ -0,0 +1,19 @@
+function addition(x, y){
+    let z = x + y;
+    return z;
+}
+
+function subtraction(x, y){
+    let z = x - y;
+    return z;
+}
+
+function multiplication(x, y){
+    let z = x * y;
+    return z;
+}
+
+function division(x, y){
+    let z = x / y;
+    return z;
+}
